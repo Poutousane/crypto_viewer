3911ebafd964728a01e889bff328d7006b7618cc
Poutousane <pitane77@gmail.com>
2025-03-14 00:09:53 +0100

    maj

--- crypto_viewer.py
+++ crypto_viewer.py
@@ -204,7 +204,7 @@ if load_button:
             metric_cols = st.columns(4)
 
             # Prix actuel formaté correctement
-            formatted_price = "${:,.2f}".format(last_price) # Alternative sans f-string
+            formatted_price = "$%.2f" % last_priceg
             metric_cols[0].markdown("""
             <div class="metric-container">
                 <div class="metric-label">Prix actuel</div>
